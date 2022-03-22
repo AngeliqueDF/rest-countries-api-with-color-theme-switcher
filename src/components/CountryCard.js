4bdcc897fcc455e0ac0c6ab35451f21e5b37c9c9
angelique-df <faye.angelique.d@gmail.com>
2022-03-22 13:10:04 +0000

    added helpers to CountryCard

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import helpers from "./../utils/helpers";
+
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
@@ -8,11 +10,13 @@ const CountryCard = ({ country }) => {
 		<li>
 			<Link to={`countries/${country.cca3}`}>
 				<Card as="article" style={{ width: "18rem" }}>
-					{/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+					<Card.Img variant="top" src={country.flags.svg} fluid="true" />
 					<Card.Title as="h2">{country.name.common}</Card.Title>
 					<Card.Body>
 						<ListGroup>
-							<ListGroup.Item>Population: {country.population}</ListGroup.Item>
+							<ListGroup.Item>
+								Population: {helpers.formatNumber(country.population)}
+							</ListGroup.Item>
 							<ListGroup.Item>Region: {country.region}</ListGroup.Item>
 							<ListGroup.Item>Capital: {country.capital}</ListGroup.Item>
 						</ListGroup>
