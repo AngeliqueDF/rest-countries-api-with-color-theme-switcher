ba810c46e8da8d59a1488ed38e3a66375021d2be
angelique-df <faye.angelique.d@gmail.com>
2022-03-29 15:00:56 +0000

    Updated CountryDetails `useEffect()`: eslint warning
    - The hook uses the `:cca3` URL parameter to find the current country to display.
    
      - First It searches into the `countries` prop.
    
        - If the page was reloaded, or the was accessed by typing the country code in the address bar, the `countries` prop is empty. `fetchCountries` now has to request the countries data from the API. The data is then used to find the country to display.
    
    - Added dependencies, to update the page once the necessary data was received.
    
    - Added a `loading` state.
    
    - Updated the component's layout.

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -5,7 +5,6 @@ import helpers from "./../utils/helpers";
 
 import GoBackButton from "./GoBackButton";
 import ListGroup from "react-bootstrap/ListGroup";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
@@ -17,14 +16,17 @@ const CountryDetails = ({ countries }) => {
 	const [countriesList, setCountriesList] = useState(countries);
 	const [country, setCountry] = useState(null);
 
-	useEffect(async () => {
+	useEffect(() => {
 		try {
 			// If we lost the countries values, we request them from the API.
-			if (countries.length === 0) {
-				const response = await countriesService.getAll();
-				setCountriesList(response.data);
+			async function fetchCountries() {
+				if (countries.length === 0) {
+					const response = await countriesService.getAll();
+					setCountriesList(response.data);
+				}
 			}
-			// Finding the current country using the URL parameter :cca3.
+			fetchCountries();
+			// Then find the current country using the URL parameter :cca3.
 			const foundCountry = countriesList.find(
 				(storedCountry) => storedCountry.cca3 === cca3
 			);
@@ -32,10 +34,10 @@ const CountryDetails = ({ countries }) => {
 		} catch (error) {
 			console.log(error);
 		}
-	}, [cca3, country]);
+	}, [cca3, country, countriesList]);
 
 	// The component might not be ready to display information at first because it's relying the network. In this case we display a loading status.
-	if (!country) {
+	if (countriesList.length === 0 || !country) {
 		return <div>loading</div>;
 	}
 
@@ -57,80 +59,100 @@ const CountryDetails = ({ countries }) => {
 	};
 	const nativeName = findNativeName(country.name.nativeName);
 	return (
-		<div>
-			<GoBackButton />
+		<>
+			<div className="country-details-container">
+				<GoBackButton />
 
-			<Row>
-				<Col>
-					<Image
-						src={country.flags.svg}
-						fluid="true"
-						alt={`Flag of ${country.name.common}`}
-					/>
-				</Col>
-
-				<Col>
-					<h2>{country.name.common}</h2>
+				<div className="country-details">
 					<Col>
-						<ListGroup.Item>Native name: {nativeName}</ListGroup.Item>
-						<ListGroup.Item>
-							Population: {helpers.formatNumber(country.population)}
-						</ListGroup.Item>
-						<ListGroup.Item>Region: {country.region}</ListGroup.Item>
-						<ListGroup.Item>Subregion: {country.subregion}</ListGroup.Item>
-						<ListGroup.Item>Capital: {country.capital}</ListGroup.Item>
+						<Image
+							src={country.flags.svg}
+							fluid="true"
+							alt={`Flag of ${country.name.common}`}
+						/>
 					</Col>
 
 					<Col>
-						<ListGroup.Item>Top level domain: {country.tld}</ListGroup.Item>
-						<ListGroup.Item>
-							<ListGroup horizontal>
-								Currencies:{" "}
-								{displayCurrencies.map((currency, index) => (
-									<ListGroup.Item key={currency.name}>
-										{helpers.listPunctuation(
-											displayCurrencies,
-											currency.name,
-											index
-										)}
-									</ListGroup.Item>
-								))}
-							</ListGroup>
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<ListGroup horizontal>
-								Languages:
-								{displayLanguages.map((language, index) => (
-									<ListGroup.Item key={language}>
-										{helpers.listPunctuation(displayLanguages, language, index)}
-									</ListGroup.Item>
-								))}
-							</ListGroup>
-						</ListGroup.Item>
-					</Col>
-					<Row>
-						{country.borders ? (
-							<ListGroup.Item>
-								<ListGroup horizontal>
-									Borders:
-									{borderCountries.map((border, index) => (
-										<ListGroup.Item key={border[0].cca3}>
-											<Link to={`/countries/${border[0].cca3}`}>
-												{helpers.listPunctuation(
-													borderCountries,
-													border[0].name.common,
-													index
-												)}
-											</Link>
-										</ListGroup.Item>
-									))}
-								</ListGroup>
+						<h2 className="country-name">{country.name.common}</h2>
+
+						<ListGroup as="ul" className="details-list">
+							<ListGroup.Item as="li">
+								<span className="bold">Native name:</span> {nativeName}
 							</ListGroup.Item>
+							<ListGroup.Item as="li">
+								<span className="bold">Population: </span>{" "}
+								{helpers.formatNumber(country.population)}
+							</ListGroup.Item>
+							<ListGroup.Item as="li">
+								<span className="bold">Region: </span> {country.region}
+							</ListGroup.Item>
+							<ListGroup.Item as="li">
+								<span className="bold">Subregion: </span> {country.subregion}
+							</ListGroup.Item>
+							<ListGroup.Item as="li">
+								<span className="bold">Capital: </span> {country.capital}
+							</ListGroup.Item>
+
+							<ListGroup.Item as="li">
+								<span className="bold">Top level domain: </span>
+								{country.tld}
+							</ListGroup.Item>
+						</ListGroup>
+
+						<ListGroup horizontal>
+							<span className="bold">Currencies:&nbsp;</span>
+							{displayCurrencies.map((currency, index) => (
+								<ListGroup.Item as="li" key={currency.name}>
+									{helpers.listPunctuation(
+										displayCurrencies,
+										currency.name,
+										index
+									)}
+								</ListGroup.Item>
+							))}
+						</ListGroup>
+
+						<ListGroup horizontal>
+							<span className="bold">Languages:&nbsp;</span>
+							{displayLanguages.map((language, index) => (
+								<ListGroup.Item as="li" key={language}>
+									{helpers.listPunctuation(displayLanguages, language, index)}
+								</ListGroup.Item>
+							))}
+						</ListGroup>
+
+						{/* Displaying the list of bordering countries if the current country has any */}
+						{country.borders ? (
+							<ListGroup as="ul" className="borders-list-container">
+								<div className="d-flex flex-column">
+									<div>
+										<h3 className="border-countries-title capitalize">
+											Borders countries:{" "}
+										</h3>{" "}
+									</div>
+									<div className="d-flex flex-wrap borders-list justify-content-start">
+										{borderCountries.map((border) => (
+											<ListGroup.Item
+												className="border-country"
+												as="li"
+												key={border[0].cca3}
+											>
+												<Link
+													className="border-country-link"
+													to={`/countries/${border[0].cca3}`}
+												>
+													{border[0].name.common}
+												</Link>
+											</ListGroup.Item>
+										))}
+									</div>
+								</div>
+							</ListGroup>
 						) : null}
-					</Row>
-				</Col>
-			</Row>
-		</div>
+					</Col>
+				</div>
+			</div>
+		</>
 	);
 };
 
