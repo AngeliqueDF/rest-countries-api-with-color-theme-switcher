821a26e61c4239b9723a659d23ca5a9929b93128
angelique-df <faye.angelique.d@gmail.com>
2022-04-05 20:41:44 +0000

    Change layout of `CountryCard`
    - Countries' information is stored in `CountryCardInfo`.
    
    - Use `CountryInfoRow` to display information.

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -3,39 +3,41 @@ import helpers from "./../utils/helpers";
 
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
+import CountryInfoRow from "./../components/CountryInfoRow";
 
 const CountryCard = ({ country }) => {
-	return (
-		<li className="country-card">
-			<Link to={`countries/${country.cca3}`}>
-				<Card as="article">
-					<Card.Img
-						variant="top"
-						src={country.flags.png}
-						fluid="true"
-						alt={`Flag of ${country.name.common}`}
-						preserveAspectRatio="none"
-					/>
-					<Card.Title as="h2">{country.name.common}</Card.Title>
-					<Card.Body>
-						<ListGroup as="ul">
-							<ListGroup.Item as="li">
-								<span className="bold">Population:</span>{" "}
-								{helpers.formatNumber(country.population)}
-							</ListGroup.Item>
-							<ListGroup.Item as="li">
-								<span className="bold">Region:</span> {country.region}
-							</ListGroup.Item>
-							<ListGroup.Item as="li">
-								<span className="bold">Capital:</span> {country.capital}
-							</ListGroup.Item>
-						</ListGroup>
-					</Card.Body>
-				</Card>
-			</Link>
-		</li>
-	);
+  const countryCardInfo = [
+    { term: "Population", value: helpers.formatNumber(country.population) },
+    { term: "Region", value: country.region },
+    { term: "Capital", value: country.capital },
+  ];
+  return (
+    <li className="country-card">
+      <Link to={`countries/${country.cca3}`}>
+        <Card as="article">
+          <Card.Img
+            variant="top"
+            src={country.flags.png}
+            fluid="true"
+            alt={`Flag of ${country.name.common}`}
+            preserveAspectRatio="none"
+          />
+          <Card.Title as="h2">{country.name.common}</Card.Title>
+          <Card.Body>
+            <dl>
+              {countryCardInfo.map((info) => (
+                <CountryInfoRow
+                  key={info.term + info.value}
+                  term={info.term}
+                  values={info.value}
+                />
+              ))}
+            </dl>
+          </Card.Body>
+        </Card>
+      </Link>
+    </li>
+  );
 };
 
 export default CountryCard;
