6f3a1f1370e29cb306b6f963331f0d0c2eb77890
angelique-df <faye.angelique.d@gmail.com>
2022-04-05 20:39:30 +0000

    Create CountryInfoRow.js
    - Displays one row of a country's information.
    
    - Works in `CountryCard` and `CountryDetails`.

---
+++ src/components/CountryInfoRow.js
@@ -0,0 +1,42 @@
+import React from "react";
+import helpers from "./../utils/helpers";
+
+const CountryInfoRow = ({ term, values }) => {
+  const DisplayValueTemplate = ({ value }) => (
+    <dd style={{ marginRight: "4px" }} key={value}>
+      {value}
+    </dd>
+  );
+
+  // Checks whether the value is an array, formats the values and displays them
+  const DisplayValue = ({ value }) => {
+    if (Array.isArray(value)) {
+      // Currencies are sent as an array of objects
+      if (term === "Currencies") {
+        return values.map((value, i) => (
+          <DisplayValueTemplate
+            key={value}
+            value={helpers.listPunctuation(values, value.name, i)}
+          />
+        ));
+      } else {
+        return value.map((v, i) => (
+          <DisplayValueTemplate
+            key={v}
+            value={helpers.listPunctuation(value, v, i)}
+          />
+        ));
+      }
+    }
+    return <DisplayValueTemplate value={value} />;
+  };
+
+  return (
+    <div className="d-flex flex-wrap country-info-row">
+      <dt className="capitalize bold">{term}:&nbsp;</dt>
+      <DisplayValue value={values} />
+    </div>
+  );
+};
+
+export default CountryInfoRow;
