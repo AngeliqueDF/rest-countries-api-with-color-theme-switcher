65f843d438748bb5357a2c74de2f251f2e043093
angelique-df <faye.angelique.d@gmail.com>
2022-04-06 18:40:51 +0000

    `prettier` formatting

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,33 +5,33 @@ import RegionFilter from "./RegionFilter";
 import CountriesList from "./CountriesList";
 
 const Home = ({
-	countries,
-	search,
-	region,
-	handleSearch,
-	handleRegionFilter,
+  countries,
+  search,
+  region,
+  handleSearch,
+  handleRegionFilter,
 }) => {
-	return (
-		<>
-			<div className="container">
-				<div className="filters">
-					<SearchCountry search={search} handleSearch={handleSearch} />
-					<RegionFilter
-						region={region}
-						handleRegionFilter={handleRegionFilter}
-					/>
-				</div>
-			</div>
+  return (
+    <>
+      <div className="container">
+        <div className="filters">
+          <SearchCountry search={search} handleSearch={handleSearch} />
+          <RegionFilter
+            region={region}
+            handleRegionFilter={handleRegionFilter}
+          />
+        </div>
+      </div>
 
-			<div className="container">
-				<CountriesList
-					countries={countries}
-					search={search}
-					regionFilter={region}
-				/>
-			</div>
-		</>
-	);
+      <div className="container">
+        <CountriesList
+          countries={countries}
+          search={search}
+          regionFilter={region}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Home;
