0b27b0ebb4338ba60d2e85ee15c5b35d273951d6
angelique-df <faye.angelique.d@gmail.com>
2022-04-19 12:47:41 +0000

    Update CountryDetails.js

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,4 +1,3 @@
-import countriesService from "./../services/countries";
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import helpers from "./../utils/helpers";
