959c724f180409eab94c30c4b3e91f146aa75f11
angelique-df <faye.angelique.d@gmail.com>
2022-07-08 20:00:44 +0000

    Style Header with `styled-components`

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,40 @@ import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--elements);
+  margin-block-end: 1rem;
+  padding: 1rem 1.6rem;
+  box-shadow: var(--light-shadow);
+  -webkit-box-shadow: var(--light-shadow);
+  -moz-box-shadow: var(--light-shadow);
+
+  h1 a {
+    font-weight: 800;
+  }
+
+  .toggle-theme {
+    color: var(--text);
+    border: none;
+    background-color: transparent;
+    padding: 0;
+    background-repeat: no-repeat;
+    background-position: left center;
+    background-size: 16px 16px;
+    padding-inline-start: 1.5rem;
+    font-size: 0.8rem;
+  }
+
+  @media only screen and (min-width: 1440px) {
+    & {
+      margin-block-end: 3rem;
+      padding: 1rem 4.5rem;
+    }
+  }
+`;
+
 const Header = ({ toggleTheme, darkThemeEnabled }) => {
   const handleToggleTheme = () => {
     toggleTheme();
